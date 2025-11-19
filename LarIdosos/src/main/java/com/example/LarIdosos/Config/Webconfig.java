3b4c0e3c4f1cd91aefc6f7c8af3ebc5ff2908d9f
Jaíne Rosa <jaine.rosa_11@live.com>
2025-11-18 22:22:00 -0300

    Atualizando Cors

--- LarIdosos/src/main/java/com/example/LarIdosos/Config/Webconfig.java
+++ LarIdosos/src/main/java/com/example/LarIdosos/Config/Webconfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Webconfig implements WebMvcConfigurer {
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://http://localhost:4200/")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
